efbf56ad28b7bfbb0e6eaf433492f28ac6773285
hirwachretien <hirwachretien2@gmail.com>
2025-05-08 16:46:22 +0200

    initial commit

---
+++ VolunteerSyncAPI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VolunteerSyncAPI.Data;
+
+namespace VolunteerSyncAPI.Controllers
+{
+    public class AuthController : ControllerBase
+    { private readonly AppDbContext _context;
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup(User user)
+        {
+            if (await _context.Users.AnyAsync)
+        }
+    }
+    {
+    }
+}
